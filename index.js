1723d81f514c8c918b34b222100a84a802fbe5e1
Muntarin Islam <s230063@dtu.dk>
2024-10-15 13:22:25 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const bicycles = require('./data/data.json');
+
+app.get('/', (req, res) => {
+    res.send(bicycles);
+});
+
+app.get('/bicycle', (req, res) => {
+   console.log(req.query.id);
+   const bicycle = bicycles.find(b=> b.id === req.query.id);
+   return res.send(bicycle);
+});
+
+app.listen(3000,() => console.log('server is running'));
