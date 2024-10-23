2825c0ea8f4e66c84ccbae2feda8729b2c7afbc7
Muntarin Islam <s230063@dtu.dk>
2024-10-23 13:45:18 +0200

    Update

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const app = express();
 
 const bicycles = require('./data/data.json');
 
+app.set('view engine','ejs');
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send(bicycles);
+   return res.render('bicycles',{
+      bicycles
+   });
 });
 
 app.get('/bicycle', (req, res) => {
-   console.log(req.query.id);
    const bicycle = bicycles.find(b=> b.id === req.query.id);
-   return res.send(bicycle);
+   return res.render('overview',{
+      bicycle
+   });
 });
 
-app.listen(3000,() => console.log('server is running'));
+app.listen(3000,() => console.log('Server is running'));
